e7d491c882338988d164399a4de4d37abd1544fe
cam <ayresc17@holyportcollege.org.uk>
2024-02-27 13:13:13 +0000

    Added (non functional) deallocating function, and updated allocater.

--- src/alloc.c
+++ src/alloc.c
@@ -1,21 +1,39 @@
+#include "alloc.h"
 /* This is the source file for the _alloc functions. */
 #include <sys/mman.h>
 
 
 /* Mushroom heap INITliser.
    Just creates a new heap, alongside all of the meta
-   data needed to work with it. */
-void *minit(int size)
+   data needed to work with it.
+   Returns a pointer to the start of this new heap. */
+int minit(heap_t *heap, int size)
 {
-    return(0x0);
-}
+    if(heap == 0x0) return(1);
 
+    heap->heapSize = size;
+    heap->heap = mmap(0x0, heap->heapSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
+
+    if(heap->heap == MAP_FAILED) return(2);
+    return(0);
+}
 
 /* MUshroom aLLOCator!
    I would have named it malloc (Mushroom ALLOCator),
    but gcc kept on throwing warnings when I did that,
-   and I couldn't be bothered to turn them off. */
+   and I couldn't be bothered to turn them off.
+   Returns a pointer to the start of the allocated area. */
 void *mulloc(int size)
 {
     return(0x0);
+}
+
+/* MUshroom DEallocator
+   Also means tired in German. */
+int mude(heap_t *heap)
+{
+    int returnCode;
+    returnCode = munmap(heap->heap, heap->heapSize);
+    
+    return(0);
 }

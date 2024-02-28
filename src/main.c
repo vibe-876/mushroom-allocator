d05eb179b9953465fd3cf9593d31696d7fadd5fa
cam <ayresc17@holyportcollege.org.uk>
2024-02-28 12:16:39 +0000

    Added some testing to main.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
+#include "alloc.h"
+
 #include <stdio.h>
 
 
 int main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
+    int errorCode;
+    heap_t myHeap;
+    
+    if((errorCode = minit(&myHeap, 64)) != 0) fprintf(stderr, "ERROR: %d\n", errorCode);
+
+    mude(&myHeap);
+    
     return(0);
 }
